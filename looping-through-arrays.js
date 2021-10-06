17aabaf77801d9d2aea856aa1d53a34e586dfef6
MustafaSKaya <mustafakya88@hotmail.com>
2021-10-06 00:00:49 +0000

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,4 @@
+pets = ['cat', 'dog', 'rat']
+for (let i = 0; i < 1; i++){
+console.log(['cats', 'dogs', 'rats'])
+}
---
+++ number-to-string.js
@@ -0,0 +1,6 @@
+let n = 128
+n = n.toString('onehundred')
+console.log(n);
+
+
+

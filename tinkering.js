b132b9f7eef47d30ddbc72d12c1285bfa86345a3
MustafaSKaya <mustafakya88@hotmail.com>
2023-01-02 19:46:24 -0500

    new challenge from leetcode

---
+++ tinkering.js
@@ -0,0 +1,16 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+var my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0. 
+var ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);
+
